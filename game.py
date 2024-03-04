c7473a64ad7a4cf01edd58fec676e9724eac869a
Patrycja Ciosek <109743645+xPatiCiosek@users.noreply.github.com>
2024-03-04 19:58:14 +0000

    x

--- game.py
+++ game.py
@@ -1,4 +1,4 @@
-from pokemon import Pokemon
+from player import Player,Comp
 import random
 
 #logic
@@ -23,42 +23,55 @@ import random
 # opponent_team = draw_comp()
 
 
-# print(f'Your pokemon:{p1.name},{p2.name},{p3.name}')
+
 # print(f'Your opponents pokemon:{p4.name},{p5.name},{p6.name}')
 
-my_choice = int(input("which pokemon you choose 1/2/3"))
+# print(player.my_team[0].name)
+# print(player.my_team[1].name)
+
+# print(comp.opponent_team[0].name)
+
+
+
+
+# my_choice = int(input("which pokemon you choose 1/2/3"))
 comp_choice = random.randint(1,3)
 
-def current_pokemon(choice,team):
-    if choice == 1:
-        current_pokemon = team[0]
-        print(f'move choices:\n{current_pokemon.moves}')
+# def current_pokemon(choice,team):
+#     if choice == 1:
+#         current_pokemon = team[0]
+#         print(f'move choices:\n{current_pokemon.moves}')
 
-    elif choice == 2:
-        current_pokemon = team[1]
-        print(f'move choices:\n{current_pokemon.moves}')
+#     elif choice == 2:
+#         current_pokemon = team[1]
+#         print(f'move choices:\n{current_pokemon.moves}')
 
-    elif choice == 3:
-        current_pokemon = team[2]
-        print(f'move choices:\n{current_pokemon.moves}')
+#     elif choice == 3:
+#         current_pokemon = team[2]
+#         print(f'move choices:\n{current_pokemon.moves}')
 
-    else:
-        print('Sorry command unvalid')
+#     else:
+#         print('Sorry command unvalid')
 
-    return current_pokemon    
+#     return current_pokemon    
 
 
-my_current_pokemon = current_pokemon(my_choice, my_team)
-comp_current_pokemon = current_pokemon(comp_choice,opponent_team)
+
+# comp_current_pokemon = current_pokemon(comp_choice,opponent_team)
 
 # print(comp_current_pokemon.name, comp_current_pokemon.hp)
 
-def move_choice(choice, pokemon):
-    if choice == 1:
-        move = pokemon.moves[0]
-    elif choice == 2:
-        move = pokemon.moves[1]
-    elif choice == 2:
-        move = pokemon.moves[2]
-    return move    
+# def move_choice(choice, pokemon):
+#     if choice == 1:
+#         move = pokemon.moves[0]
+#     elif choice == 2:
+#         move = pokemon.moves[1]
+#     elif choice == 2:
+#         move = pokemon.moves[2]
+#     return move    
+
+
+# if my_current_pokemon.hp <0:
+#     print('Your pokemon:')
+#     print('Whcih pokemon do you wan tto choose now? 1/2')
 
