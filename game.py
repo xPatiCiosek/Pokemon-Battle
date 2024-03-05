b56cd4ac2b2bb76fcc04e21afbbc3a7ecf8902e0
Patrycja Ciosek <109743645+xPatiCiosek@users.noreply.github.com>
2024-03-05 17:31:43 +0000

    1-2-1 created game class

--- game.py
+++ game.py
@@ -1,77 +1,12 @@
-from player import Player,Comp
-import random
-
-#logic
-
-
-# p1 = Pokemon()
-# p2 = Pokemon()
-# p3 = Pokemon()
-
-# p4 = Pokemon()
-# p5 = Pokemon()
-# p6 = Pokemon()
-
-# def draw():
-#     return p1,p2,p3
-
-# def draw_comp():
-#     return p4,p5,p6
-
-
-# my_team = draw()
-# opponent_team = draw_comp()
-
-
-
-# print(f'Your opponents pokemon:{p4.name},{p5.name},{p6.name}')
-
-# print(player.my_team[0].name)
-# print(player.my_team[1].name)
-
-# print(comp.opponent_team[0].name)
-
-
-
-
-# my_choice = int(input("which pokemon you choose 1/2/3"))
-comp_choice = random.randint(1,3)
-
-# def current_pokemon(choice,team):
-#     if choice == 1:
-#         current_pokemon = team[0]
-#         print(f'move choices:\n{current_pokemon.moves}')
-
-#     elif choice == 2:
-#         current_pokemon = team[1]
-#         print(f'move choices:\n{current_pokemon.moves}')
-
-#     elif choice == 3:
-#         current_pokemon = team[2]
-#         print(f'move choices:\n{current_pokemon.moves}')
-
-#     else:
-#         print('Sorry command unvalid')
-
-#     return current_pokemon    
-
-
-
-# comp_current_pokemon = current_pokemon(comp_choice,opponent_team)
-
-# print(comp_current_pokemon.name, comp_current_pokemon.hp)
-
-# def move_choice(choice, pokemon):
-#     if choice == 1:
-#         move = pokemon.moves[0]
-#     elif choice == 2:
-#         move = pokemon.moves[1]
-#     elif choice == 2:
-#         move = pokemon.moves[2]
-#     return move    
-
-
-# if my_current_pokemon.hp <0:
-#     print('Your pokemon:')
-#     print('Whcih pokemon do you wan tto choose now? 1/2')
-
+class Game:
+    def __init__(self, player, opponent):
+        self.player = player
+        self.opponent = opponent
+    
+    def play(self):
+        # Player moves first
+        damage = self.player.make_next_move()
+        self.opponent.current_pokemon.hp -= damage
+        # Opponent moves next
+        damage = self.opponent.make_next_move()
+        self.player.current_pokemon.hp -= damage
