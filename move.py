4ba9200b01f070850a5a9ef8d776aecb54bbea60
Patrycja Ciosek <109743645+xPatiCiosek@users.noreply.github.com>
2024-03-05 17:08:13 +0000

    indentation fix

--- move.py
+++ move.py
@@ -8,20 +8,20 @@ class Move:
         self.name = move_data['name']
         self.type = move_data['type']['name']
         self.power = move_data['power']
-        self.accuracy = move_data['accuracy']
+        # self.accuracy = move_data['accuracy']
 
 
-        @staticmethod
-        def assign_moves(move_list):
-            moves_assigned = []
-            move_data_list = []
-            if len(move_list) < 3:
-                moves_assigned = move_list
-            else:
-                moves_assigned = random.sample(move_list, k=3)
+    @staticmethod
+    def assign_moves(move_list):
+        moves_assigned = []
+        move_data_list = []
+        if len(move_list) < 3:
+            moves_assigned = move_list
+        else:
+            moves_assigned = random.sample(move_list, k=3)
 
-            for move in moves_assigned:
-                move_request = Request(MOVES.format(move))
-                move_data = move_request.get_content()
-                move_data_list.append(Move(move_data)) 
-            return move_data_list
+        for move in moves_assigned:
+            move_request = Request(MOVES.format(move))
+            move_data = move_request.get_content()
+            move_data_list.append(Move(move_data)) 
+        return move_data_list
