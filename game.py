c826c0bd90b5b7cda32b1b42aa4cc0690fe6b92e
Patrycja Ciosek <109743645+xPatiCiosek@users.noreply.github.com>
2024-03-10 22:16:35 +0000

    created first_move_to() dependant on pokemon.speed

--- game.py
+++ game.py
@@ -4,19 +4,24 @@ class Game:
         self.opponent = opponent
 
     def play(self):  
+        if self.opponent.current_pokemon.speed > self.player.current_pokemon.speed: 
+            # First move belongs to opponent
+            self.first_move_to(self.opponent,self.player)
+        else:
+            # First move belongs to player
+            self.first_move_to(self.player, self.opponent)
+
+    def first_move_to(self, first_player ,second_player): 
         while True:
-            # Player moves first
-            self.attack(self.player, self.opponent)
-            if self.defeat_checker(self.opponent):
+            self.attack(first_player, second_player)
+            if self.defeat_checker(second_player):
                 print('Congrats! You won!!!')
                 break
-
-            # Opponent moves next
-            self.attack(self.opponent, self.player)
-            if self.defeat_checker(self.player):
+            
+            self.attack(second_player, first_player)
+            if self.defeat_checker(first_player):
                 print('Oh no! Your opponent won this time')
-                break
-
+                break           
 
     @staticmethod
     def attack(attacker, defender):
@@ -25,11 +30,11 @@ class Game:
         print(f"{defender.current_pokemon.name}'s hp: {defender.current_pokemon.hp}\n")
 
     @staticmethod
-    def defeat_checker(user):
-        if user.current_pokemon.hp <= 0:
-                user.remove_pokemon()
-                if len(user.my_team) > 0:
-                    user.set_new_current_pokemon()
+    def defeat_checker(player):
+        if player.current_pokemon.hp <= 0:
+                player.remove_pokemon()
+                if len(player.my_team) > 0:
+                    player.set_new_current_pokemon()
                     return False
                 else:
                     return True
