61c04bfde49316f89fba97de9437afc298a72274
Patrycja Ciosek <109743645+xPatiCiosek@users.noreply.github.com>
2024-03-10 15:39:49 +0000

    created and implemented get_power def

--- move.py
+++ move.py
@@ -7,9 +7,15 @@ class Move:
     def __init__(self, move_data):
         self.name = move_data['name']
         self.type = move_data['type']['name']
-        self.power = move_data['power']
+        self.power = self.get_power(move_data)
+        
+    @staticmethod
+    def get_power(move_data):
         if move_data['power'] is None:
-            self.power = 0
+            power = 0
+        else:
+            power = move_data['power']
+        return power    
 
     @staticmethod
     def assign_moves(move_list):
