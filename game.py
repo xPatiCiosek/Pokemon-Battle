3887bf59ab5f73f9664f5c3c3ba6cab787a65ee4
Patrycja Ciosek <109743645+xPatiCiosek@users.noreply.github.com>
2024-03-10 15:15:48 +0000

    put logic of the game & wrapped it in while loop

--- game.py
+++ game.py
@@ -2,11 +2,33 @@ class Game:
     def __init__(self, player, opponent):
         self.player = player
         self.opponent = opponent
-    
-    def play(self):
-        # Player moves first
-        damage = self.player.make_next_move()
-        self.opponent.current_pokemon.hp -= damage
-        # Opponent moves next
-        damage = self.opponent.make_next_move()
-        self.player.current_pokemon.hp -= damage
+
+    def play(self):  
+        while True:
+            # Player moves first
+            damage = self.player.make_next_move(self.opponent)
+            self.opponent.current_pokemon.hp -= damage
+            print(f"{self.opponent.current_pokemon.name}'s hp: {self.opponent.current_pokemon.hp}\n")
+
+            if self.opponent.current_pokemon.hp <= 0:
+                self.opponent.remove_pokemon()
+                if len(self.opponent.my_team) > 0:
+                    self.opponent.set_new_current_pokemon()
+                else:
+                    print("you won")
+                    break;    
+
+            # Opponent moves next
+            damage = self.opponent.make_next_move(self.player)
+            self.player.current_pokemon.hp -= damage
+
+            print(f"{self.player.current_pokemon.name}'s hp: {self.player.current_pokemon.hp}")
+            if self.player.current_pokemon.hp <= 0:
+                self.player.remove_pokemon()
+                if len(self.player.my_team) > 0:
+                    self.player.set_new_current_pokemon()
+                else:
+                    print("your opponent won")
+                    break; 
+        
+   
