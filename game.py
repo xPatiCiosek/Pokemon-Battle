6b020ae5e95ecde7b3ce97fde416c7ba9ec2a360
Patrycja Ciosek <109743645+xPatiCiosek@users.noreply.github.com>
2024-03-10 21:24:07 +0000

    created and implemented defeat_checker def

--- game.py
+++ game.py
@@ -7,28 +7,30 @@ class Game:
         while True:
             # Player moves first
             self.attack(self.player, self.opponent)
-
-            if self.opponent.current_pokemon.hp <= 0:
-                self.opponent.remove_pokemon()
-                if len(self.opponent.my_team) > 0:
-                    self.opponent.set_new_current_pokemon()
-                else:
-                    print("you won")
-                    break;    
+            if self.defeat_checker(self.opponent):
+                print('Congrats! You won!!!')
+                break
 
             # Opponent moves next
             self.attack(self.opponent, self.player)
-
-            if self.player.current_pokemon.hp <= 0:
-                self.player.remove_pokemon()
-                if len(self.player.my_team) > 0:
-                    self.player.set_new_current_pokemon()
-                else:
-                    print("your opponent won")
-                    break; 
+            if self.defeat_checker(self.player):
+                print('Oh no! Your opponent won this time')
+                break
 
 
-    def attack(self, attacker, defender):
+    @staticmethod
+    def attack(attacker, defender):
         damage = attacker.make_next_move(defender)
         defender.current_pokemon.hp -= damage
         print(f"{defender.current_pokemon.name}'s hp: {defender.current_pokemon.hp}\n")
+
+    @staticmethod
+    def defeat_checker(user):
+        if user.current_pokemon.hp <= 0:
+                user.remove_pokemon()
+                if len(user.my_team) > 0:
+                    user.set_new_current_pokemon()
+                    return False
+                else:
+                    return True
+                
